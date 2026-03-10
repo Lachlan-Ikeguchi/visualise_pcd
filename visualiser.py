96f9b5c023ad9164daa65eba41c07f8978339a62
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-03-10 20:24:04 +1000

    feat(scan_2): added scan_2 for testing

--- visualiser.py
+++ visualiser.py
@@ -1,4 +1,15 @@
 #! /usr/bin/env python3.11
 
 import open3d as o3d
+import sys
 
+files = sys.argv[1:]
+
+for file in files:
+    point_cloud = o3d.io.read_point_cloud(file)
+    
+    point_cloud.estimate_normals()
+
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud, depth=9)
+
+    o3d.visualization.draw_plotly([mesh])   
