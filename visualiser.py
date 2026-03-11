44459ddaff438b23d39c561765da64af05da4b19
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-03-11 22:16:33 +1000

    feat(plot): added a way to increase the size of the viewport

--- visualiser.py
+++ visualiser.py
@@ -10,6 +10,9 @@ FILTER_ITERATIONS = 1
 UPSIDE_DOWN = True
 POINT_OVERLAY = True
 
+PLOT_WIDTH = 1200
+PLOT_HEIGHT= 800
+
 for file in files:
     point_cloud = o3d.io.read_point_cloud(file)
     
@@ -25,21 +28,6 @@ for file in files:
 
     mesh.filter_smooth_laplacian(number_of_iterations=FILTER_ITERATIONS)
 
-    # remove poorly-defined huge faces (often from open regions/unbounded areas)
-    # compute triangle areas manually since Open3D lacks a direct helper
-    verts = np.asarray(mesh.vertices)
-    tris = np.asarray(mesh.triangles)
-    if tris.size > 0:
-        v0 = verts[tris[:, 0]]
-        v1 = verts[tris[:, 1]]
-        v2 = verts[tris[:, 2]]
-        cross = np.cross(v1 - v0, v2 - v0)
-        areas = 0.5 * np.linalg.norm(cross, axis=1)
-        thresh = areas.mean() + 3 * areas.std()
-        mask = areas < thresh
-        mesh.remove_triangles_by_mask(~mask)
-        mesh.remove_unreferenced_vertices()
-
     if POINT_OVERLAY:
         # subsampled points for visualisation
         display_cloud = point_cloud.random_down_sample(0.1)
@@ -47,7 +35,7 @@ for file in files:
     mesh.compute_vertex_normals()
 
     if POINT_OVERLAY:
-        o3d.visualization.draw_plotly([mesh, display_cloud])
+        o3d.visualization.draw_plotly([mesh, display_cloud], width=PLOT_WIDTH , height=PLOT_HEIGHT)
     else:
-        o3d.visualization.draw_plotly([mesh])
+        o3d.visualization.draw_plotly([mesh], width=PLOT_WIDTH , height=PLOT_HEIGHT)
         
