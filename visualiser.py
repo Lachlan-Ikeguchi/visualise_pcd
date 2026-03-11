fea3435d83f09dba28f39d7c53c4a6abd81e2b33
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-03-11 23:30:56 +1000

    feat(filtering): added additional filtering, smoothing, and render improvements

--- visualiser.py
+++ visualiser.py
@@ -44,12 +44,30 @@ for file in files:
         ]
         edge_lengths.append(max(edges))
 
-    # Remove triangles with edge lengths greater than threshold
     large_triangle_mask = np.array(edge_lengths) > FILTER_LENGTH_THRESHOLD
     mesh.remove_triangles_by_mask(large_triangle_mask)
     mesh.remove_unreferenced_vertices()
+    mesh.remove_degenerate_triangles()
+    mesh.remove_duplicated_triangles()
+    mesh.remove_duplicated_vertices()
+    mesh.remove_non_manifold_edges()
+
+    triangle_labels, cluster_sizes, _ = mesh.cluster_connected_triangles()
+    triangle_labels = np.asarray(triangle_labels)
+    num_triangles = len(mesh.triangles)
+
+    triangles_to_remove_mask = np.zeros(num_triangles, dtype=bool)
+
+    for i, size in enumerate(cluster_sizes):
+        if size < 3:
+            triangles_to_remove_mask[triangle_labels == i] = True
+
+    if np.any(triangles_to_remove_mask):
+        mesh.remove_triangles_by_mask(triangles_to_remove_mask)
+        mesh.remove_unreferenced_vertices()
 
     mesh.filter_smooth_laplacian(number_of_iterations=FILTER_ITERATIONS)
+    mesh.filter_smooth_taubin(number_of_iterations=FILTER_ITERATIONS)
 
     if POINT_OVERLAY:
         # subsampled points for visualisation
@@ -57,9 +75,37 @@ for file in files:
 
     mesh.compute_vertex_normals()
 
+    mesh_bounds = mesh.get_axis_aligned_bounding_box()
+
+    min_bound = mesh_bounds.min_bound
+    max_bound = mesh_bounds.max_bound
+
+    z_range = max_bound[2] - min_bound[2]
+
+    # expand z range to make it flatter
+    z_expansion = z_range * 2.0
+
+    from open3d.geometry import TriangleMesh
+    bounding_points = np.array([
+        [min_bound[0], min_bound[1], min_bound[2] - z_expansion],  # Bottom
+        [max_bound[0], min_bound[1], min_bound[2] - z_expansion],
+        [min_bound[0], max_bound[1], min_bound[2] - z_expansion],
+        [max_bound[0], max_bound[1], min_bound[2] - z_expansion],
+        [min_bound[0], min_bound[1], max_bound[2] + z_expansion],  # Top
+        [max_bound[0], min_bound[1], max_bound[2] + z_expansion],
+        [min_bound[0], max_bound[1], max_bound[2] + z_expansion],
+        [max_bound[0], max_bound[1], max_bound[2] + z_expansion],
+    ])
+
+    bounding_box_mesh = TriangleMesh()
+    bounding_box_mesh.vertices = o3d.utility.Vector3dVector(bounding_points)
+    bounding_box_mesh.paint_uniform_color([1, 1, 1])  # invisible
+
     if POINT_OVERLAY:
         o3d.visualization.draw_plotly(
-            [mesh, display_cloud], width=PLOT_WIDTH, height=PLOT_HEIGHT)
+            [mesh, display_cloud, bounding_box_mesh],
+            width=PLOT_WIDTH, height=PLOT_HEIGHT)
     else:
         o3d.visualization.draw_plotly(
-            [mesh], width=PLOT_WIDTH, height=PLOT_HEIGHT)
+            [mesh, bounding_box_mesh],
+            width=PLOT_WIDTH, height=PLOT_HEIGHT)
